20e8c21032f3db83991fb63875b7d6978de85f95
Laura Cros Vila <lcrosvila@gmail.com>
2025-07-26 11:29:03 +0200

    update all

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/csrd-compliance-manager/', // 👈 required for GitHub Pages
+  base: '/csrd-compliance-manager/',
   plugins: [react()],
 });
